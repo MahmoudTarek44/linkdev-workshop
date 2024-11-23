c430f9e7522b53b227c1f18a25c729bee53374f4
Mahmoud <mahmoudtarekmahmoud44@gmail.com>
2024-11-24 00:46:37 +0200

    update tailwindConfig, add colors, add typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,20 @@ const tailwindConfig = {
   ],
   theme: {
     extend: {
-      colors: {},
-      typography: {}
+      colors: {
+        primary: '#FF7C16',
+        secondary: '#0ECEFF',
+        purple: '#F215B6',
+        light_green: "#A6E51C",
+        light_gray: '#74747433',
+        demo_red: '#E72228',
+        gradiant_bg: '#D5AEE4',
+      },
+      typography: {
+        poppins: ["Poppins", 'serif'],
+        open_sans: ["Open Sans", 'serif'],
+        volkhov: ["Volkhov", 'serif'],
+      }
     },
   },
   plugins: [
